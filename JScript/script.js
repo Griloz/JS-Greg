eedce6a59c278ff6dfa09ed044e7e989c2235799
GriLoz <grilozev@gmail.com>
2022-12-13 23:18:59 -0500

    first commit

---
+++ JScript/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+alert ('Hello');
+
+// const result = confirm("Are u here?");
+// console.log (result);
+
+
+// const answer= prompt ("Вам есть 18?", " ");
+// console.log (answer+5);
+// console.log (typeof(answer));
+
+
+// const answers = [];
+
+// answers [0] = prompt ('Whats u name?', '');
+// answers [1] = prompt ('Whats u surname?', '');
+// answers [2] = prompt ('Where are u live?', '');
+
+// document.write (answers);
+// console.log (typeof(answers));
