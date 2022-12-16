c5c7b752f2d6acd774255438e182a1397ae0a367
GriLoz <grilozev@gmail.com>
2022-12-15 21:15:50 -0500

    add sample 23-40

--- JScript/script.js
+++ JScript/script.js
@@ -25,3 +25,24 @@
 // console.log (typeof(answers));
 
 // 23-40
+'use strict';
+
+
+const numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?','');
+
+const personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+};
+ 
+const a = prompt ('Один из последних просмотренных фильмов?',''),
+      b = prompt ('На сколько оцените его?',''),
+      c = prompt ('Один из последних просмотренных фильмов?',''),
+      d = prompt ('На сколько оцените его?','');
+ personalMovieDB.movies[a] = b;
+ personalMovieDB.movies[c] = d;
+
+ console.log (personalMovieDB);
